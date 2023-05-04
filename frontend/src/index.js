04769d2972f874e3816f205aa59ee88b3d85acd8
ItsSosha <whoissosha@gmail.com>
2023-05-04 18:51:07 +0200

    set up router and global styles

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createGlobalStyle } from 'styled-components';
+import { 
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom';
 import App from './App';
+
+const Global = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+`
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+
+    ]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Global />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
