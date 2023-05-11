05affc2fd463a6b6be734df778573cb45e4dcbf7
cutebunchofpixels <poroshkinanthony18@gmail.com>
2023-05-11 20:41:49 +0300

    PriceHistoryChart component

--- frontend/src/pages/ProductAbout.js
+++ frontend/src/pages/ProductAbout.js
@@ -1,14 +1,24 @@
 import { useOutletContext } from "react-router-dom";
 import styled from "styled-components";
-import { Typography, Paper, Link } from "@mui/material";
+import { Typography, Paper, Link, Box } from "@mui/material";
 import SpecificationsList from "../components/SpecificationsList/SpecificationsList";
+import PriceHistoryChart from "../components/PriceHistoryChart";
 
 const AboutWrapper = styled.div`
 padding: 20px;
 display: flex;
 flex-direction: column;
 `
+const samplePriceHistory = [
+  {date: "2023-05-06", price: 32500},
+  {date: "2023-05-07", price: 33500},
+  {date: "2023-05-08", price: 33500},
+  {date: "2023-05-09", price: 30000},
+  {date: "2023-05-10", price: 30000},
+  {date: "2023-05-11", price: 32500}
+];
 
+const IS_USER_PREMIUM = true;
 
 
 const ProductAbout = () => {
@@ -34,7 +44,9 @@ const ProductAbout = () => {
         }}>
         Динаміка цін
       </Typography>
-      <Paper
+      
+      {(!IS_USER_PREMIUM) 
+      ? <Paper
         sx={{
           display: "flex",
           justifyContent: "center",
@@ -50,6 +62,12 @@ const ProductAbout = () => {
           Для доступу до динаміки цін необхідно <Link href="#">оформити підписку</Link>
           </Typography>
       </Paper>
+      : <Box sx={{
+        width: "100%",
+        height: "100%",
+      }}>
+        <PriceHistoryChart data={samplePriceHistory}/>
+      </Box>}
     </AboutWrapper>
   )
 }
