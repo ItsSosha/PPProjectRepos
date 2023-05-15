d6ddfa27c2a86c110e859ddda93bb849cbff98b7
osigram <igor.b769@gmail.com>
2023-05-15 20:53:38 +0300

    Added raw UserRepository implementation

--- Backend/Core/User.cs
+++ Backend/Core/User.cs
@@ -3,7 +3,9 @@
 public class User : BaseEntity
 {
     public string Email { get; set; }
-    public string AuthKey { get; set; }
+    public string FirstName { get; set; }
+    public string Surname { get; set; }
+    public string PictureLink { get; set; }
     public bool IsAdmin { get; set; }
     public IList<UserItem> Favourites { get; set; }
 }
---
+++ Backend/DataLayer/Abstract/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿using Core;
+
+namespace DataLayer.Abstract;
+
+public interface IUserRepository
+{
+    public Task<User?> GetOrRegisterUser(string jwt);
+}
